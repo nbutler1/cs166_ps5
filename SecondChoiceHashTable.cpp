de302fc2349b01d1b2e5ab9658ae0f7c7c074148
Nathan <nathanbutler@DN0a226796.SUNet>
2018-05-21 13:57:55 -0700

    Init Second choice hashing

--- SecondChoiceHashTable.cpp
+++ SecondChoiceHashTable.cpp
@@ -1,22 +1,60 @@
 #include "SecondChoiceHashTable.h"
 
 SecondChoiceHashTable::SecondChoiceHashTable(size_t numBuckets, std::shared_ptr<HashFamily> family) {
-  // TODO: Implement this
+  for(size_t i = 0; i < numBuckets; i++){
+    b1.push_back({});
+    b2.push_back({});
+  }
+  numBucks = numBuckets;
+  h1 = family->get();
+  h2 = family->get();
 }
 
 SecondChoiceHashTable::~SecondChoiceHashTable() {
   // TODO: Implement this
 }
 
+
 void SecondChoiceHashTable::insert(int data) {
-  // TODO: Implement this
+  if(contains(data))
+    return;
+  size_t buck1 = h1(data) % numBucks;
+  size_t buck2 = h2(data) % numBucks;
+  if(b1[buck1].size() < b2[buck2].size()){
+      b1[buck1].push_back(data);
+  }else{
+      b2[buck2].push_back(data);
+  }
 }
 
 bool SecondChoiceHashTable::contains(int data) const {
-  // TODO: Implement this
+  size_t buck1 = h1(data) % numBucks;
+  size_t buck2 = h2(data) % numBucks;
+  for(size_t i = 0; i < b1[buck1].size(); i++){
+    if(b1.at(buck1).at(i) == data)
+        return true;
+  } 
+  for(size_t i = 0; i < b2[buck2].size(); i++){
+    if(b2.at(buck2).at(i) == data)
+        return true;
+  } 
   return false;
 }
 
 void SecondChoiceHashTable::remove(int data) {
   // TODO: Implement this
+  size_t buck1 = h1(data) % numBucks;
+  size_t buck2 = h2(data) % numBucks;
+  for(size_t i = 0; i < b1[buck1].size(); i++){
+    if(b1.at(buck1).at(i) == data){
+        b1.at(buck1).erase(b1.at(buck1).begin() + i);
+        return;
+    }
+  } 
+  for(size_t i = 0; i < b2[buck2].size(); i++){
+    if(b2.at(buck2).at(i) == data){
+        b2.at(buck2).erase(b2.at(buck2).begin() + i);
+        return;
+    }
+  } 
 }
--- SecondChoiceHashTable.h
+++ SecondChoiceHashTable.h
@@ -44,15 +44,17 @@ public:
   void remove(int key);
   
 private:
-  /* TODO: Add any data members or private helper functions that you'll need,
-   * then delete this comment.
-   */
   
   /* Fun with C++: these next two lines disable implicitly-generated copy
    * functions that would otherwise cause weird errors if you tried to
    * implicitly copy an object of this type. You don't need to touch these
    * lines.
    */
+  size_t numBucks;
+  std::vector<std::vector<int> > b1;
+  std::vector<std::vector<int> > b2;
+  HashFunction h1;
+  HashFunction h2;
   SecondChoiceHashTable(SecondChoiceHashTable const &) = delete;
   void operator=(SecondChoiceHashTable const &) = delete;
 };
